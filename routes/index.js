cbd23dc49199c62779bab8715b70fdc665aabe48
Ashay Mishra <ashaymishra30111998@gmail.com>
2020-10-01 11:22:09 +0000

    add env pw

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ var upload = multer({ storage: storage, fileFilter: imageFilter})
 
 var cloudinary = require('cloudinary');
 cloudinary.config({ 
-  cloud_name: 'ashaycloud', 
+  cloud_name: process.env.CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
 	// api_key: '843864198355499',
